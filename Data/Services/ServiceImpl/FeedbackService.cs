3c08fc92f516fa02ea20bb3d6ecb98c29558b797
emqiu <quy0584602657@gmail.com>
2025-04-25 01:53:59 +0700

    doantotnghiep

---
+++ Data/Services/ServiceImpl/FeedbackService.cs
@@ -0,0 +1,38 @@
+﻿using Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services.ServiceImpl
+{
+    public class FeedbackService : IFeedbackService
+    {
+        TGClothesDbContext db = null;
+        public FeedbackService()
+        {
+            db = new TGClothesDbContext();
+        }
+
+        public bool Delete(long id)
+        {
+            var data = db.Feedbacks.Find(id);
+            db.Feedbacks.Remove(data);
+            db.SaveChanges();
+            return true;
+        }
+
+        public List<Feedback> GetAll()
+        {
+            return db.Feedbacks.ToList();
+        }
+
+        public long InsertFeedback(Feedback feedback)
+        {
+            db.Feedbacks.Add(feedback);
+            db.SaveChanges();
+            return feedback.Id;
+        }
+    }
+}
